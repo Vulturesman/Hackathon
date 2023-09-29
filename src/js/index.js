31e6c0c5f8c7d2dbc1fb27baded0b6bcdbd25484
Vanessa Blaho <vanessadblaho@gmail.com>
2023-09-29 15:10:43 +0200

    added my form to Jakub's awesome modal

--- src/js/index.js
+++ src/js/index.js
@@ -81,3 +81,49 @@ need to create for every button:
 extra overlay div for whole body
 <div id="darken"></div>
 */
+
+
+///start of form ///
+
+console.log("ok");
+
+const submitButton = document.getElementsByClassName('registration-button-submit');
+
+const eventId = 1
+//event I want to register to - needs to be based on ID of the event that brought up the registration form //
+const registerForm = document.getElementById('register-form')
+console.log(registerForm);
+
+registerForm.addEventListener('submit',async (event)=> {
+    event.preventDefault()
+    const urlReg = `https://test-api.codingbootcamp.cz/api/bcc04bd8/events/${eventId}/registrations`
+    // 1 - make this real
+    const radioButtons = document.querySelectorAll('input[name=age]')
+   
+    let selectedAge = null
+    radioButtons.forEach(button => {
+        if (button.checked) {
+            selectedAge = button.value
+        }
+    })
+
+    const registrationData = {
+        "First Name": registerForm.querySelector('input[name=firstName]').value,
+        "Last name": registerForm.querySelector('input[name=lname]').value,
+        "email":registerForm.querySelector('input[name=email]').value,
+        "Phone Number":registerForm.querySelector('input[name=phoneNumber]').value,
+        "age": selectedAge
+    };
+
+    const myResponse = await fetch(urlReg, {
+      "method": "POST",
+      "body": JSON.stringify(registrationData),
+      "headers": {
+        'Content-Type': 'application/json'
+      }
+    })
+    const myUsableResponse = await myResponse.json()
+    console.log(myUsableResponse)
+  });
+
+  /////end of form ///
