ba7501778b235cc8a67152239decf13adf0ab7fe
David Štafa <david.stafa@gmail.com>
2023-09-29 15:05:57 +0200

    final upravy

--- src/js/class.js
+++ src/js/class.js
@@ -2,13 +2,15 @@ const getEvents = async () => {
   const url = "https://test-api.codingbootcamp.cz/api/bcc04bd8/events";
   const data = await fetch(url);
   const response = await data.json();
+
+  console.log(response[0].id);
   response.forEach((event) => {
     new OtherEvents(event);
   });
 };
 
 getEvents();
-const boxForMainevent = document.querySelector("");
+const boxForMainevent = document.querySelector(".main-event");
 const boxForEvents = document.querySelector(".main-other-events");
 
 class OtherEvents {
@@ -17,20 +19,41 @@ class OtherEvents {
     this.element = document.createElement("div");
     this.name = event.name;
     this.image = event.image_url;
+    this.desciption = event.description;
+    this.id = event.id;
     this.elementNew();
   }
 
   elementNew = () => {
-    if (event.id == 0) {
-    } else {
-    }
-
-    boxForEvents.appendChild(this.element);
-    this.element.innerHTML = `
-        <h3>${this.name}</h3>
-        <button class="register-button main-other-event__button">MORE</button>
+    if (this.id == 1) {
+        console.log(this.id)
+        boxForMainevent.appendChild(this.element);
+        this.element.className = "main-event__craeted-box";
+        this.element.innerHTML = `
+        <div class="main-event__featured">
+          <p>FEATURED EVENT</p>
+        </div>
+        <div class="main-event__img">
+          <img src="${this.image}" alt="event" />
+        </div>
+        <div class="main-event__description">
+          <h2>${this.name}</h2>
+          <h3>${this.desciption}</h3>
+        </div>
+        <div class="main-event__registration">
+          <button class="main-event__registration__button register-button">
+            Register
+          </button>
+        </div>
         `;
-    this.element.className = "main-other-events__box";
-    this.element.style.backgroundImage = `url(${this.image})`;
+    } else {
+        boxForEvents.appendChild(this.element);
+        this.element.innerHTML = `
+            <h3>${this.name}</h3>
+            <button class="register-button main-other-event__button">MORE</button>
+            `;
+        this.element.className = "main-other-events__box";
+        this.element.style.backgroundImage = `url(${this.image})`;
+    }
   };
 }
